1fd8f34d1f10d215d193a6f77b494c8ec39a7b21
SHOIB ANSARI <reddishshoib123@gmail.com>
2024-06-01 12:46:03 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create the deployment
-kubectl apply deployment -f pod-deploy.yaml
+kubectl apply -f pod-deploy.yaml
 
 # Create the service
-kubectl apply service -f pod-service.yaml
+kubectl apply -f pod-service.yaml
 
 # Check for service status (optional)
 kubectl get service
